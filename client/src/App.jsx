1b335aaebcf6b34aad17c19ec5b80ad214b0445b
Aman850-ux <amanraghuwanshi873@gmail.com>
2025-10-06 20:37:50 +0530

    feat: Refactor user dashboard and profile components; remove unused dashboard route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,6 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
 import UserDashbaord from "./pages/UserDashbaord";
 
 const App = () => {
@@ -22,7 +21,6 @@ const App = () => {
           <Route path={"/contact"} element={<Contact />} />
           <Route path={"/login"} element={<Login />} />
           <Route path={"/register"} element={<Register />} />
-           <Route path={"/dashboard"} element={<Dashboard />} />
             <Route path={"/userDashboard"} element={<UserDashbaord />} />
           
         </Routes>
--- server/src/config/cloudinary.js
+++ server/src/config/cloudinary.js
@@ -1,10 +1,15 @@
-import dotenv from "dotenv";
-dotenv.config();
+// import dotenv from "dotenv";
+// dotenv.config();
 
-import {v2} from 'cloudinary';
+// import {v2} from 'cloudinary';
 
-v2.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
-})
+// v2.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET,
+// })
+
+
+// CLOUDINARY_CLOUD_NAME=dvsl56zii
+// CLOUDINARY_API_KEY=262985448146296
+// CLOUDINARY_API_SECRET=DtSMV5tW1nApSlUCbbhryUS3e84
