49a72d4c1369a9922ae61eef074f52edad0ad548
Aman850-ux <amanraghuwanshi873@gmail.com>
2025-10-08 19:51:57 +0530

    feat: Implement user profile update functionality; add profile picture preview and validation

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import connectDB from "./src/config/db.js";
 import AuthRouter from "./src/routers/authRouter.js";
+import UserRouter from "./src/routers/userRoutes.js";
 import morgan from "morgan";
 
 const app = express();
@@ -17,6 +17,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 
 app.use("/auth", AuthRouter);
+app.use("/user", UserRouter);
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome to Job Portal API" });
